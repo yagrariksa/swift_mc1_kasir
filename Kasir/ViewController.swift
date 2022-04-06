94a6ecfeb78d39a36046aec1b52a09347102bb74
yagrariksa <daffa.yagrariksa@gmail.com>
2022-04-06 14:44:58 +0700

    Initial Commit

---
+++ Kasir/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kasir
+//
+//  Created by Daffa Yagrariksa on 06/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
